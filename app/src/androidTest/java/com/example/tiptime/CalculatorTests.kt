1cf692bb4003db3648b14a998efc698aaa7e06be
Juan Lopez Vallejo <juanmi.lopez@gruposantander.com>
2022-03-05 12:02:59 +0100

    Complete all tests

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -34,4 +34,73 @@ class CalculatorTests {
         onView(withId(R.id.tip_result))
             .check(matches(withText(containsString("10.00"))))
     }
+
+    @Test
+    fun calculate_different_percents() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+        // To show button correctly
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.calculate_button)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.00"))))
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("9.00"))))
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("8.00"))))
+
+        onView(withId(R.id.option_twenty_percent)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.00"))))
+    }
+
+    @Test
+    fun calculate_round() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("52.00"))
+        // To show button correctly
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.calculate_button)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("11.00"))))
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.40"))))
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("11.00"))))
+    }
+
+    @Test
+    fun calculate_round_percents() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("52.00"))
+        // To show button correctly
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.calculate_button)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("11.00"))))
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.40"))))
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("9.36"))))
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("7.80"))))
+
+        onView(withId(R.id.option_twenty_percent)).perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.40"))))
+    }
 }
